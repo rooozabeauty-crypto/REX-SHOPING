c30d621a199604c2b1d554946ef54984a8c115c8
رموز احمد <rooozabeauty@gmail.com>
2026-04-04 04:40:55 +0300

    Add TypeScript types to callback handler

--- Api/callback.ts
+++ Api/callback.ts
@@ -1,4 +1,6 @@
-export default async function handler(req, res) {
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { code } = req.query;
 
   if (!code) {
