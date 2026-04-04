d2197eb01061e3e938d1d5607ef65d372fd2442d
رموز احمد <rooozabeauty@gmail.com>
2026-04-04 19:03:48 +0300

    Update callback.ts

--- Api/callback.ts
+++ Api/callback.ts
@@ -23,7 +23,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
         client_id: "7e42b932-6690-477d-aba0-a9fca78047e5",
         client_secret: "fb00757f191f6e75ea747a4cefbaf6b977c0b187c9060b4caab2e4367409dce5",
         code: code,
-        redirect_uri: "https://rex-shop-4.vercel.app/api/callback"
+        redirect_uri: "https://nex-r-shopping.onhercules.app/api/callback"
       })
     });
 
