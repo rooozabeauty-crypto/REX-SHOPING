14a3c2f72fc349e33bf588695abfc226ca5ff488
رموز احمد <rooozabeauty@gmail.com>
2026-04-04 08:30:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ const express = require('express');
 const fetch = require('node-fetch');
 
 const app = express();
-
-// مهم جدًا لقراءة JSON من سلة
 app.use(express.json());
 
-// الصفحة الرئيسية (اختياري)
+// الصفحة الرئيسية
 app.get('/', (req, res) => {
-  res.send('🚀 Rex Shop API is running');
+  res.send('🚀 Rex Shop API يعمل بنجاح');
 });
 
-
-// 🔐 OAuth Callback من سلة
+// 🔗 OAuth Callback (سلة)
 app.get('/api/callback', async (req, res) => {
   const code = req.query.code;
 
@@ -21,4 +18,15 @@ app.get('/api/callback', async (req, res) => {
   }
 
   try {
-    const response = await
+    const response = await fetch('https://accounts.salla.sa/oauth2/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        grant_type: 'authorization_code',
+        client_id: process.env.CLIENT_ID, 7e42b932-6690-477d-aba0-a9fca78047e5
+        client_secret: process.env.CLIENT_SECRET, fb00757f191f6e75ea747a4cefbaf6b977c0b187c9060b4caab2e4367409dce5
+        code: code,
+        redirect_uri: 'https://rex-shop-4.onrender.com/api/callback'
+      }),
